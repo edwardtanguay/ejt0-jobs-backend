473156bc32fd2dde4540af0fccc909f4d4c30d90
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-23 07:07:17 +0200

    added dynamic port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const port = 3021;
+const port = process.env.PORT;
 app.use(cors());
 
 app.get('/jobs', (req, res) => {
